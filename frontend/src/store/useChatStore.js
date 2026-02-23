d78be42df6af8c32841dc3525abf0aa1770c6567
Kartik Sharma <kartiksharma4418@gmail.com>
2026-02-23 20:28:08 +0530

    final updations before deployment

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -6,8 +6,7 @@ import { useAuthStore } from "./useAuthStore";
 export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
-  selectedUser:
-  JSON.parse(localStorage.getItem("selected-chat-user")) || null,
+  selectedUser: null,
   isUsersLoading: false,
   isMessagesLoading: false,
 
@@ -45,36 +44,25 @@ export const useChatStore = create((set, get) => ({
   },
 
   subscribeToMessages: () => {
-  const { selectedUser } = get();
-  if (!selectedUser) return;
+    const { selectedUser } = get();
+    if (!selectedUser) return;
 
-  const socket = useAuthStore.getState().socket;
-  if (!socket) return;
+    const socket = useAuthStore.getState().socket;
 
-  socket.off("newMessage");
+    socket.on("newMessage", (newMessage) => {
+      const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
+      if (!isMessageSentFromSelectedUser) return;
 
-  socket.on("newMessage", (newMessage) => {
-    const isMessageSentFromSelectedUser =
-      newMessage.senderId?.toString() === selectedUser._id?.toString();
-
-    if (!isMessageSentFromSelectedUser) return;
-
-    set({
-      messages: [...get().messages, newMessage],
+      set({
+        messages: [...get().messages, newMessage],
+      });
     });
-  });
-},
+  },
 
   unsubscribeFromMessages: () => {
     const socket = useAuthStore.getState().socket;
     socket.off("newMessage");
   },
 
-  setSelectedUser: (selectedUser) => {
-  localStorage.setItem(
-    "selected-chat-user",
-    JSON.stringify(selectedUser)
-  );
-  set({ selectedUser });
-},
+  setSelectedUser: (selectedUser) => set({ selectedUser }),
 }));
